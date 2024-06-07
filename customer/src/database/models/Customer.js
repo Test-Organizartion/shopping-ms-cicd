84a08c5acacfacdb145f1488cc14ae8322c3f64b
HuzaifaAsifKhan <huzaifaasifkhan@hotmail.com>
2024-06-07 14:26:17 +0400

    First Commit

---
+++ customer/src/database/models/Customer.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+    email: String,
+    password: String,
+    salt: String,
+    phone: String,
+    address:[
+        { type: Schema.Types.ObjectId, ref: 'address', require: true }
+    ],
+    cart: [
+        {
+          product: {
+            _id: {type: String, required: true},
+            name: {type: String},
+            desc: {type: String},
+            banner: {type: String},
+            type: {type: String},
+            unit: {type: Number},
+            price: {type: Number},
+            available: {type: Boolean},
+            suplier: {type: String}
+        },
+        unit: { type: Number, require: true}
+        }
+    ],
+    wishlist:[
+        { 
+            _id: {type: String, required: true},
+            name: {type: String},
+            desc: {type: String},
+            banner: {type: String},
+            type: {type: String},
+            unit: {type: Number},
+            price: {type: Number},
+            available: {type: Boolean},
+            suplier: {type: String}
+        }
+    ],
+    orders: [ 
+        { 
+            _id: {type: String, required: true},
+            amount: {type: Number},
+            date: {type: Date, default: Date.now()}
+        }
+    ]
+},{
+    toJSON: {
+        transform(doc, ret){
+            delete ret.password;
+            delete ret.salt;
+            delete ret.__v;
+        }
+    },
+    timestamps: true
+});
+
+module.exports =  mongoose.model('customer', CustomerSchema);
---
+++ product/src/config/index.js
@@ -0,0 +1,20 @@
+const dotEnv = require("dotenv");
+const path = require('path');
+if (process.env.NODE_ENV !== "prod") {
+  const configFile = `../../.env.dev`;
+  // dotEnv.config({ path: path.resolve(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+  dotEnv.config({ path: path.resolve(__dirname, configFile) });
+} else {
+  dotEnv.config();
+}
+
+module.exports = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.MONGODB_URI,
+  APP_SECRET: process.env.APP_SECRET,
+  MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
+  EXCHANGE_NAME: 'ONLINE_SHOPPING',
+  QUEUE_NAME:'PRODUCT_QUEUE',
+  SHOPPING_BINDING_KEY: 'SHOPPING_SERVICE',
+  CUSTOMER_BINDING_KEY: 'CUSTOMER_SERVICE'
+};
