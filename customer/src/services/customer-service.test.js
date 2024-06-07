8879b65f4e479e54b53ad0d2a49a17de7e677e21
HuzaifaAsifKhan <huzaifaasifkhan@hotmail.com>
2024-06-07 17:24:26 +0400

    Commit add

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,14 @@
+// Which Service it is
+describe('Customer Service', () => {
+    // which function it is
+    describe('SignIn', () => {
+        // which scenario it is
+        test('validate input', () => {
+
+        })
+
+        test('validate response', () => {
+
+        })
+    })
+})
