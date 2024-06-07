70f87988dae6cfc4904bd673321918804afa0c6f
Huzaifa Asif <48243280+HuzaifaAsifKhan@users.noreply.github.com>
2024-06-07 21:32:12 +0400

    first Pull request (#1)
    * Commit add
    
    * Version changes
    
    * test Case remove
    
    * Gateway test case remove
    
    * Change test to jest from workflow file

---
+++ product/src/services/product-service.test.js
@@ -0,0 +1,14 @@
+// Which Service it is
+describe('Product Service', () => {
+    // which function it is
+    describe('CreateProduct', () => {
+        // which scenario it is
+        test('validate input', () => {
+
+        })
+
+        test('validate response', () => {
+
+        })
+    })
+})
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,14 @@
+// Which Service it is
+describe('Shopping Service', () => {
+    // which function it is
+    describe('PlaceOrder', () => {
+        // which scenario it is 😁
+        test('validate input', () => {
+
+        })
+
+        test('validate response', () => {
+
+        })
+    })
+})
