a02c0852507b404e1dfa2934ca170d217003f02b
HuzaifaAsifKhan <huzaifaasifkhan@hotmail.com>
2024-06-11 13:50:46 +0400

    Change file for prod

--- customer/src/config/index.js
+++ customer/src/config/index.js
@@ -5,7 +5,8 @@ if (process.env.NODE_ENV === "dev") {
   // dotEnv.config({ path: path.resolve(__dirname, `../../.env.${process.env.NODE_ENV}`) });
   dotEnv.config({ path: path.resolve(__dirname, configFile) });
 } else {
-  dotEnv.config();
+  const configFile = `../../.env`;
+  dotEnv.config({ path: path.resolve(__dirname, configFile) });
 }
 module.exports = {
   PORT: process.env.PORT,
